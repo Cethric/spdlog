f1983d79eff5f1e1b717164ba07e080352421f5a
Blake Rogan <roganblake@gmail.com>
2020-03-29 15:18:20 +1100

    Fix dependencies

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -33,7 +33,7 @@ function(spdlog_enable_warnings target_name)
             -Wall -Wextra -Wconversion -pedantic -Wfatal-errors>
         $<$<CXX_COMPILER_ID:MSVC>:/W3>)
 		if(MSVC_VERSION GREATER  1900)  #Allow non fatal security wanrnings for msvc 2015
-			target_compile_options(${target_name} PRIVATE /WX)
+#			target_compile_options(${target_name} PRIVATE /WX)
 		endif()
 endfunction()
 
